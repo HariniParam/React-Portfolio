2243a1c4ff3f6a6919b29dfd76ba78e5eab6bfce
HariniParam <21pt05@psgtech.ac.in>
2023-12-04 20:09:40 +0530

    Added About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
     <section id="about">
-      About
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className="about__me-image">
+            <img src={ME} alt="About Image"/>
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Skills</h5>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+            </article>
+          </div>
+          <p>
+            Hey there!I'm Harini, a dedicated and passionate individual deeply enthusiastic about frontend web development.
+            I'm currently pursuing a Master of Science in Theoretical computer Science 
+            at PSG College of Technology.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
