a7a255d988e4cd535976027232f8f7cfa2ed8ea9
HariniParam <21pt05@psgtech.ac.in>
2023-12-07 22:17:46 +0530

    Updated portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jfif'
-import IMG2 from '../../assets/portfolio2.png'
+import IMG2 from '../../assets/portfolio2.jfif'
 import IMG3 from '../../assets/portfolio3.jfif'
 import IMG4 from '../../assets/portfolio4.jfif'
 import IMG5 from '../../assets/portfolio5.jfif'
