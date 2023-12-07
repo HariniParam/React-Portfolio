06b20ed125daa4ff154aea7730c5c65166abeec4
HariniParam <21pt05@psgtech.ac.in>
2023-12-07 22:16:54 +0530

    Updated Nav Bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './nav.css'
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
-import { BiBook } from "react-icons/bi";
+import { FaAward } from 'react-icons/fa'
 import { BiMessageSquareDetail } from "react-icons/bi"
+import { GrProjects } from "react-icons/gr";
 import { useState } from 'react';
 
 const Nav = () => {
@@ -12,7 +13,8 @@ const Nav = () => {
    <nav>
     <a href="#" onClick={()=>setActiveNav('#')} className={ activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
     <a href="#about" onClick={()=>setActiveNav('#about')} className={ activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-    <a href="#skills" onClick={()=>setActiveNav('#skills')} className={ activeNav === '#skills' ? 'active' : ''}><BiBook /></a>
+    <a href="#skills" onClick={()=>setActiveNav('#skills')} className={ activeNav === '#skills' ? 'active' : ''}><FaAward /></a>
+    <a href="#portfolio" onClick={()=>setActiveNav('#portfolio')} className={ activeNav === '#portfolio' ? 'active' : ''}><GrProjects /></a>
     <a href="#contact"onClick={()=>setActiveNav('#contact')} className={ activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
    </nav>
   )
