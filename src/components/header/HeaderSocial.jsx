c6b75cc283570a88988bf405142e17896cf7cd13
HariniParam <21pt05@psgtech.ac.in>
2023-12-07 21:00:42 +0530

    updated HeaderSocial.jsx

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
-import { SiLeetcode } from "react-icons/si";
+import { FaInstagram } from "react-icons/fa";
 
 const HeaderSocial = () => {
   return (
     <div className='header__socials'>
-      <a href="www.linkedin.com/in/harinip3007" target="_blank"><BsLinkedin /></a>
+      <a href="https://www.linkedin.com/in/harinip3007/" target="_blank"><BsLinkedin /></a>
       <a href="https://github.com/HariniParam" target="_blank"><FaGithub /></a>
-      <a href="https://leetcode.com/Harini_hp/" target="_blank"><SiLeetcode /></a>
+      <a href="https://www.instagram.com/hariniii.___/?hl=en" target="_blank"><FaInstagram /></a>
     </div>
   )
 }
