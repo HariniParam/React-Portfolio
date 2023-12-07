cfa10fe995fa4918be85b688e001ea6a62e65f23
HariniParam <21pt05@psgtech.ac.in>
2023-12-07 22:26:37 +0530

    Updated about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,6 +3,7 @@ import './about.css'
 import ME from '../../assets/about.jpg'
 import { FaAward } from 'react-icons/fa'
 import { VscFolderLibrary } from 'react-icons/vsc'
+import { PiStudentBold } from "react-icons/pi";
 
 const About = () => {
   return (
@@ -17,19 +18,27 @@ const About = () => {
         </div>
         <div className='about__content'>
           <div className='about__cards'>
+          <article className='about__card'>
+              <PiStudentBold className='about__icon'/>
+              <h5>Student</h5>
+              <small>3rd Year M.sc Theoretical Computer Science at PSG College of Technology</small>
+            </article>
             <article className='about__card'>
               <FaAward className='about__icon'/>
-              <h5>Skills</h5>
+              <h5>Experience</h5>
+              <small>Currently an intern as web developer at Prodigy InfoTech</small>
             </article>
             <article className='about__card'>
               <VscFolderLibrary className='about__icon'/>
               <h5>Projects</h5>
+              <small>10+ completed</small>
             </article>
           </div>
           <p>
-            Hey there!I'm Harini, a dedicated and passionate individual deeply enthusiastic about frontend web development.
-            I'm currently pursuing a Master of Science in Theoretical computer Science 
-            at PSG College of Technology.
+            Hey there! I'm Harini, a dedicated and passionate individual deeply enthusiastic about frontend web development.
+            My dedication to learning fuels my continuous exploration of new technologies and design trends within web development and design.
+            Proficient in HTML, CSS, and JavaScript, I specialize in creating engaging and responsive user interfaces. 
+            I'm deeply intrigued by the fusion of technology and creativity, exploring the design aspects of frontend development to craft intuitive and visually appealing web experiences.
           </p>
           <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
